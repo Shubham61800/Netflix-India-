344d07d1c11514ed2099d3126f9b22edd28d42fe
Shubham61800 <106237435+Shubham61800@users.noreply.github.com>
2022-11-03 09:37:40 +0530

    add js

---
+++ index.js
@@ -0,0 +1,10 @@
+let item1=document.getElementById('pr1');
+function toggleinfo1(){
+    if(item1.style.display==='none')
+    {
+        item1.style.display='block';
+    }
+    else{
+     (item1.style.display='none');
+    }
+}
